9dbb78264a255fc0457a6808a297bd273ab36b34
Tomasiinho <l_carrasco@alumnos.upla.cl>
2025-03-22 14:40:42 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Formulario</title>
+</head>
+<body>
+    <fieldset>
+        <legend>¿Qué habilidades te interesan?</legend>
+        <label><input type="checkbox" name="habilidad" value="programacion"> Programación</label>
+        <label><input type="checkbox" name="habilidad" value="musica"> Música</label>
+        <label><input type="checkbox" name="habilidad" value="idiomas"> Idiomas</label>
+    </fieldset>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
